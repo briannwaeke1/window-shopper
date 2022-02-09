f22bca29c08da115040aeccb21fac718d524b28d
Brian Nwaeke <bnwaeke@icloud.com>
2022-02-08 22:32:02 -0800

    styling menu-item component

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,20 +1,21 @@
+
 import React from 'react';
+
 import './menu-item.styles.scss';
 
 const MenuItem = ({ title, imageUrl, size }) => (
-    // Dynamically change styles (background image) for each menu item component to the string interpolated value of image URL
-    <div style={{ 
+  <div className={`${size} menu-item`}>
+    <div
+      className='background-image'
+      style={{
         backgroundImage: `url(${imageUrl})`
-    }}
-    // Dynamically change class name value. If size value exists, add it to the class name
-    className={`${size} menu-item`}
-    >
-        <div className='content'>
-            <h1 className='title'>{title}</h1>
-            <span className='subtitle'>SHOP NOW</span>
-        </div>
-    </div>    
+      }}
+    />
+    <div className='content'>
+      <h1 className='title'>{title.toUpperCase()}</h1>
+      <span className='subtitle'>SHOP NOW</span>
+    </div>
+  </div>
 );
-
 
 export default MenuItem;
