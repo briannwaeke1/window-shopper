362914451ae56833e46186bd1754a28582f20dd4
Brian Nwaeke <bnwaeke@icloud.com>
2022-02-11 12:27:59 -0800

    create firebase project, add firebase config file and set up google auth for sign-in component

---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,31 @@
+// Import the functions needed from the SDKs
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/firestore';
+import 'firebase/compat/auth';
+
+// Firebase configuration
+const config = {
+  apiKey: 'AIzaSyCWB59lSLYl3jEul6Nhtv-_BMJztZ2rO0w',
+  authDomain: 'window-shopper-db.firebaseapp.com',
+  projectId: 'window-shopper-db',
+  storageBucket: 'window-shopper-db.appspot.com',
+  messagingSenderId: '1096913043902',
+  appId: '1:1096913043902:web:d597a9e65fa5f35256b9ef',
+  measurementId: 'G-NB0CGPENGV',
+};
+
+// Initialize Firebase
+firebase.initializeApp(config);
+
+// Export authentication and firebase library to be used in App
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+// Set up Google authentication utility which gives us access to the Google auth provider class from the auth library.
+const provider = new firebase.auth.GoogleAuthProvider();
+// Set up custom parameter to always trigger the google pop up whenever this google auth provider is used for authentication and sign-on
+provider.setCustomParameters({ prompt: 'select_account' });
+// Only export sign in with google method
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+export default firebase;
