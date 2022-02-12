239f54f91ee8a3691ad51df5d27b446a40e8dfbf
Brian Nwaeke <bnwaeke@icloud.com>
2022-02-12 00:16:22 -0800

    created sign-up component, created firestore collection for users

--- src/App.js
+++ src/App.js
@@ -5,46 +5,56 @@ import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
-import { auth } from './firebase/firebase.utils';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
 class App extends React.Component {
-  constructor() {
-    super();
+	constructor() {
+		super();
 
-    this.state = {
-      currentUser: null,
-    };
-  }
+		this.state = {
+			currentUser: null
+		};
+	}
 
-  unsubscribeFromAuth = null;
+	unsubscribeFromAuth = null;
 
-  // Subscribe to the open messaging system between the app and the firebase app.
-  // Firebase notifies the app of any auth changes.
-  componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged((user) => {
-      this.setState({ currentUser: user });
+	// Subscribe to the open messaging system between the app and the firebase app.
+	// Firebase notifies the app of any auth changes.
+	componentDidMount() {
+		this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+			if (userAuth) {
+				const userRef = await createUserProfileDocument(userAuth);
 
-      console.log(user);
-    });
-  }
+				userRef.onSnapshot(snapShot => {
+					this.setState({
+						currentUser: {
+							id: snapShot.id,
+							...snapShot.data()
+						}
+          });
+				});
+			}
+			this.setState({ currentUser: userAuth });
+		});
+	}
 
-  // Unsubscribe from the open messaging system when unmounted to prevent memory leaks.
-  componentWillUnmount() {
-    this.unsubscribeFromAuth();
-  }
+	// Unsubscribe from the open messaging system when unmounted to prevent memory leaks.
+	componentWillUnmount() {
+		this.unsubscribeFromAuth();
+	}
 
-  render() {
-    return (
-      <div>
-        <Header currentUser={this.state.currentUser} />
-        <Switch>
-          <Route exact path='/' component={HomePage} />
-          <Route path='/shop' component={ShopPage} />
-          <Route path='/signin' component={SignInAndSignUpPage} />
-        </Switch>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div>
+				<Header currentUser={this.state.currentUser} />
+				<Switch>
+					<Route exact path='/' component={HomePage} />
+					<Route path='/shop' component={ShopPage} />
+					<Route path='/signin' component={SignInAndSignUpPage} />
+				</Switch>
+			</div>
+		);
+	}
 }
 
 export default App;
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -5,13 +5,37 @@ import 'firebase/compat/auth';
 
 // Firebase configuration
 const config = {
-  apiKey: 'AIzaSyCWB59lSLYl3jEul6Nhtv-_BMJztZ2rO0w',
-  authDomain: 'window-shopper-db.firebaseapp.com',
-  projectId: 'window-shopper-db',
-  storageBucket: 'window-shopper-db.appspot.com',
-  messagingSenderId: '1096913043902',
-  appId: '1:1096913043902:web:d597a9e65fa5f35256b9ef',
-  measurementId: 'G-NB0CGPENGV',
+	apiKey: 'AIzaSyCWB59lSLYl3jEul6Nhtv-_BMJztZ2rO0w',
+	authDomain: 'window-shopper-db.firebaseapp.com',
+	projectId: 'window-shopper-db',
+	storageBucket: 'window-shopper-db.appspot.com',
+	messagingSenderId: '1096913043902',
+	appId: '1:1096913043902:web:d597a9e65fa5f35256b9ef',
+	measurementId: 'G-NB0CGPENGV'
+};
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+	if (!userAuth) return;
+	const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+	const snapShot = await userRef.get();
+	if (!snapShot.exists) {
+		const { displayName, email } = userAuth;
+		const createdAt = new Date();
+
+		try {
+			await userRef.set({
+				displayName,
+				email,
+				createdAt,
+				...additionalData
+			});
+		} catch (error) {
+			console.log('error creating user', error.message);
+		}
+	}
+
+	return userRef;
 };
 
 // Initialize Firebase
