22e5c266c943b088a3fc788ecee0ee37f1a6c33b
Brian Nwaeke <bnwaeke@icloud.com>
2022-02-19 03:32:28 -0800

    installed react-persist library and set up persist config to allow for persistance of redux store

--- src/index.js
+++ src/index.js
@@ -3,13 +3,18 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import store from './redux/store';
+import { store, persistor } from './redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
 import './index.css';
+
+/* Wrap the root App component with PersistGate which delays the rendering of the app’s UI until your persisted state has been retrieved and saved to redux. */
 
 ReactDOM.render(
 	<Provider store={store}>
 		<BrowserRouter>
-			<App />
+			<PersistGate persistor={persistor}>
+				<App />
+			</PersistGate>
 		</BrowserRouter>
 	</Provider>,
 	document.getElementById('root')
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,13 @@
-import {createStore, applyMiddleware} from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import { persistStore } from 'redux-persist';
 import logger from 'redux-logger';
 import rootReducer from './root-reducer';
 
 const middlewares = [logger];
 
-const store = createStore(rootReducer, applyMiddleware(...middlewares));
+export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
-export default store;
+// The persistStore ensures the redux state is saved to persisted storage whenever it changes
+export const persistor = persistStore(store);
 
 
